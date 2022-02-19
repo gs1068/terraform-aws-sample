c854de816e07b0e2a0466006682abc5c69c3f551
gs1068 <genta.otias@gmail.com>
2022-02-19 21:37:20 +0900

    [add]SGのアウトプット設定

--- src/modules/security_group/output.tf
+++ src/modules/security_group/output.tf
@@ -4,3 +4,9 @@ output "sg_alb_id" {
 output "sg_rds_id" {
   value = aws_security_group.rds.id
 }
+output "sg_eks_master_id" {
+  value = aws_security_group.eks-master.id
+}
+output "sg_eks_node_id" {
+  value = aws_security_group.eks-node.id
+}
