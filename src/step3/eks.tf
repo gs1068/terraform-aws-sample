756f6aaf39f69308fa9f395ceb71d922ecfd494a
gs1068 <genta.otias@gmail.com>
2022-02-20 20:06:19 +0900

    [add]dockerfileにGItをインストール

--- src/step3/eks.tf
+++ src/step3/eks.tf
@@ -1,10 +1,12 @@
 module "eks" {
   source       = "./../modules/eks"
+  k8s_version  = "1.21"
   service_name = var.service_name
-  version = "14.0.0"
-
-  worker_groups = [{
-    root_volume_type = "gp2"
-    target_group_arns = [aws_alb_target_group.tg.arn]
-  }]
+  subnet_ids = [
+    data.terraform_remote_state.step1.outputs.public_subnet_a_id,
+    data.terraform_remote_state.step1.outputs.public_subnet_c_id,
+    data.terraform_remote_state.step1.outputs.public_subnet_d_id
+  ]
+  vpc_id            = data.terraform_remote_state.step1.outputs.vpc
+  target_group_arns = data.terraform_remote_state.step1.outputs.eks_alb_tg
 }
