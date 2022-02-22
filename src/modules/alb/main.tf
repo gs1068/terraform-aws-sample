30f81a92ccf0c8d395ff242ec025fb49636ff5ad
gs1068 <genta.otias@gmail.com>
2022-02-22 09:00:10 +0900

    [fix]EKSのターゲットグループ修正

--- src/modules/alb/main.tf
+++ src/modules/alb/main.tf
@@ -19,23 +19,6 @@ resource "aws_alb_target_group" "alb_eks" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 }
-# resource "aws_alb_target_group" "alb_eks" {
-#   name     = "${terraform.workspace}-tg-http"
-#   port     = 8080
-#   protocol = "HTTP"
-#   vpc_id   = "${var.vpc_id}"
-#   target_type = "ip"
-
-#   health_check {
-#     interval            = 60
-#     path                = "/"
-#     port                = 8080  
-#     protocol            = "HTTP"
-#     timeout             = 20
-#     unhealthy_threshold = 4
-#     matcher             = 200
-#   }
-# }
 
 resource "aws_alb_listener" "alb_http" {
   load_balancer_arn = aws_alb.alb_eks.arn
